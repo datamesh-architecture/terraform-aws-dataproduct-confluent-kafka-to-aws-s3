97a5e6a46057cc1a45fe0e19abaf3f7aa946f67a
André Deuerling <andre.deuerling@innoq.com>
2023-03-02 09:10:27 +0100

    Updated required providers

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.56"
     }
     confluent = {
       source  = "confluentinc/confluent"
-      version = "1.24.0"
+      version = ">= 1.34"
     }
   }
 }

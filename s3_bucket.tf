012490093adeaf60e88d32000deb79d45f0434f5
André Deuerling <andre.deuerling@innoq.com>
2023-03-06 10:38:31 +0100

    Enabled bucket encryption

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -9,6 +9,22 @@ resource "aws_s3_bucket_acl" "aws_s3_bucket_acl" {
   acl    = "private"
 }
 
+resource "aws_kms_key" "aws_kms_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_server_side_encryption_configuration" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.aws_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
   bucket = aws_s3_bucket.aws_s3_bucket.id
   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
